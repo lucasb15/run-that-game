34f8b235391bd352119e3c56a01aa43cdf223aa1
Lucas Gomes Santos <lgsantos306@gmail.com>
2025-01-30 15:18:41 -0300

    chore: retirado arquivos desnecessarios e comeco do css global

---
+++ src/components/BackgroundCircles/BackgoundCircles.jsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+const BackgroundCircles = () => {
+  return (
+    <div className="backgound-circulos">
+      <div className="circulos-grandes">
+        <div className="circulo-grande"></div>
+        <div className="circulo-grande"></div>
+        <div className="circulo-grande"></div>
+      </div>
+    </div>
+  );
+};
+
+export default BackgroundCircles;
